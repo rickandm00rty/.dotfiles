8e421a82e60c659eb7143f4ddb4bf357d18f803c
Kyle <kylenelson272@gmail.com>
2022-04-26 19:24:43 -0400

    Updated Installer

--- install.sh
+++ install.sh
@@ -4,8 +4,9 @@
 echo "Installing Dependencies & Programs...Configuring Files...."
 sudo dnf -y install feh conky picom polybar scrot rofi imwheel xfce4-terminal zsh neovim tmux thunar gnome-control-center gnome-software neofetch
 
-mkdir ~/.config/i3 ~/.config/nvim
-mkdir ~/.config/i3/polybar
+mkdir ~/.config/i3 
+mkdir ~/.config/nvim
+mkdir ~/.config/polybar
 
 cp -r ~/dotfiles/Xorg/ ~/
 cp -r ~/dotfiles/i3/* ~/.config/i3/
@@ -19,13 +20,12 @@ cp -r ~/dotfiles/polybar ~/.config/polybar/
 echo "Adding MS Edge & Steam RPM's..."
 
 #EDGE
-sudo rpm —import https://packages.microsoft.com/keys/microsoft.asc
-sudo dnf config-manager —add-repo https://packages.microsoft.com/yumrepos/edge
-sudo dnf update —refresh
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo dnf config-manager --add-repo https://packages.microsoft.com/yumrepos/edge
+sudo dnf update --refresh
 sudo dnf install -y microsoft-edge-stable
 
 # STEAM
-sudo dnf install -y https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-35.noarch.rpm
 sudo dnf install -y steam
 
 #ZSH/Oh-My-Zsh
