2c91929c91d371173d589795eacc1cf2dee434c8
m0rtydump <73741302+rickandm00rty@users.noreply.github.com>
2022-05-03 12:58:17 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,30 +2,32 @@
 
 
 echo "Installing Dependencies & Programs...Configuring Files...."
-sudo dnf -y install feh conky picom polybar scrot rofi imwheel xfce4-terminal zsh neovim tmux thunar gnome-control-center gnome-software neofetch
+sudo dnf -y install feh conky picom scrot rofi imwheel xfce4-terminal zsh neovim tmux thunar gnome-control-center gnome-software neofetch
 
+echo "Making Directories and Copying Binaries"
+mkdir ~/Downloads
+mkdir ~/Pictures ~/Pictures/Wallpapers/
 mkdir ~/.config/i3 
+mkdir ~/.config/i3status
 mkdir ~/.config/nvim
-mkdir ~/.config/polybar
+mkdir ~/.config/i3status
 
-cp -r ~/dotfiles/Xorg/ ~/
 cp -r ~/dotfiles/i3/* ~/.config/i3/
+cp -r ~/dotfiles/i3status/* ~/.config/i3status/
 cp -r ~/dotfiles/nvim/* ~/.config/nvim/
 cp -r ~/dotfiles/tmux/.tmux.conf  ~/
 cp -r ~/dotfiles/vim/.vimrc ~/
 cp -r ~/dotfiles/ZSH/.zshrc ~/
-cp -r ~/dotfiles/polybar ~/.config/polybar/
-
-# Dependencies
-echo "Adding MS Edge & Steam RPM's..."
 
 #EDGE
+echo "Adding MS Edge & Steam RPM's..."
+
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo dnf config-manager --add-repo https://packages.microsoft.com/yumrepos/edge
 sudo dnf update --refresh
 sudo dnf install -y microsoft-edge-stable
 
-# STEAM
+#STEAM
 sudo dnf install -y steam
 
 #ZSH/Oh-My-Zsh
@@ -34,5 +36,6 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 chsh -s $(which zsh)
 
 echo "Grabbing Obsidian .appImage and AppImageLauncher"
-#wget “url” and dependencies
+cd ~/Downloads
+wget https://github.com/TheAssassin/AppImageLauncher/releases/download/v2.2.0/appimagelauncher-2.2.0-travis995.0f91801.x86_64.rpm and dependencies
 
