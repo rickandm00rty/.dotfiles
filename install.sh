4444dfd0277942bdf061dde8f24bb28b14e98a79
m0rtydump <73741302+rickandm00rty@users.noreply.github.com>
2022-05-04 10:06:59 -0600

    Adding imwheel support
    Mouse Scrolling is slow, or otherwise not controllable in GNOME or i3. Thank you to Nick Norton for providing this script to integrate with imwheel to allow for custom scrolling speed.

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 
-echo "Installing Dependencies & Programs...Configuring Files...."
+echo "Installing Dependencies & Programs..."
+echo "Configuring Files..."
 sudo dnf -y install feh conky picom scrot rofi imwheel xfce4-terminal zsh neovim tmux thunar gnome-control-center gnome-software neofetch
 
-echo "Making Directories and Copying Binaries"
+echo "Making Directories and Copying Binaries..."
 mkdir ~/Downloads
 mkdir ~/Pictures ~/Pictures/Wallpapers/
 mkdir ~/.config/i3 
@@ -32,13 +33,19 @@ sudo dnf install -y microsoft-edge-stable
 sudo dnf install -y steam
 
 #ZSH/Oh-My-Zsh
-echo "Getting that cool shell stuff and setting default to ZSH"
+echo "Getting that cool shell stuff and setting default to ZSH..."
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 chsh -s $(which zsh)
 
-echo "Grabbing Obsidian and Configuring AppImageLauncher"
+echo "Grabbing Obsidian and Configuring AppImageLauncher..."
 cd ~/Downloads
 wget https://github.com/TheAssassin/AppImageLauncher/releases/download/v2.2.0/appimagelauncher-2.2.0-travis995.0f91801.x86_64.rpm
 dnf install -y ./appimagelauncher-2.2.0-travis995.0f91801.x86_64.rpm
+
+echo "Adjusting Scrolling..."
+bash <(curl -s http://www.nicknorton.net/mousewheel.sh)
+chmod +x mousewheel.sh
+
+./mousewheel.sh
 
 
